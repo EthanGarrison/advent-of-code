8a35094e419917ef5437b79a2f08905943d4b033
Ethan Garrison <ethan.garrison@publicisgroupe.com>
2019-12-03 15:07:05 -0500

    Convert Day02 maps to vectors because using maps for index ops is stupid

--- src/main/scala/com/ethang/adventofcode/y2019/Day02.scala
+++ src/main/scala/com/ethang/adventofcode/y2019/Day02.scala
@@ -5,54 +5,50 @@ import com.ethang.adventofcode.readResourceFile
 object Day02 {
 
   private val partOneAnswer = 3101878
+  private val partTwoAnswer = 8444
 
-  private def evalOpCode(curr: Int, listing: Map[Int, Int]): Map[Int, Int] = {
-//    println(s"Listing given: \n${listing.mkString("\n")}")
+  private def evalOpCode(curr: Int, listing: Vector[Int]): Vector[Int] = {
     val opCode = listing(curr)
     val leftArg = listing(curr + 1)
     val rightArg = listing(curr + 2)
     val assignLoc = listing(curr + 3)
-//    println(s"Evaluating op code $opCode: $assignLoc = $leftArg ${if(opCode == 1) "+" else "*"} $rightArg")
 
     val updatedValue = opCode match {
       case 1 => listing(leftArg) + listing(rightArg)
       case 2 => listing(leftArg) * listing(rightArg)
     }
 
-    listing + (assignLoc -> updatedValue)
+    listing.updated(assignLoc, updatedValue)
   }
 
-  def evalProgram(programListing: Map[Int, Int]): Map[Int, Int] = {
+  def evalProgram(programListing: Vector[Int]): Vector[Int] = {
     @scala.annotation.tailrec
-    def recurse(listing: Map[Int, Int], line: Int): Map[Int, Int] = {
+    def recurse(listing: Vector[Int], line: Int): Vector[Int] = {
       if(listing(line) == 99) listing
       else recurse(evalOpCode(line, listing), line + 4)
     }
     recurse(programListing, 0)
   }
 
-  def partOne(opCodeList: Seq[Int]): Int = {
-    evalProgram(opCodeList.zipWithIndex.map(_.swap).toMap ++ Map(1 -> 12, 2 -> 2))(0)
+  def partOne(opCodeList: Vector[Int]): Int = {
+    evalProgram(opCodeList.updated(1, 12).updated(2, 2))(0)
   }
 
-  def partTwo(opCodeList: Seq[Int]): Int = {
-    val programListing = opCodeList.zipWithIndex.map(_.swap).toMap
-
+  def partTwo(opCodeList: Vector[Int]): Int = {
     val nounVerbList = for(n <- (0 to 99).iterator; v <- (0 to 99).iterator) yield (n, v)
 
     nounVerbList
-      .find { case (n, v) => evalProgram(programListing ++ Map(1 -> n, 2 -> v))(0) == 19690720 }
+      .find { case (n, v) => evalProgram(opCodeList.updated(1, n).updated(2, v))(0) == 19690720 }
       .map { case (n, v) => 100 * n + v}
       .get
   }
 
   def runAll(inputPath: String): Unit = {
-    val inputData = readResourceFile(inputPath).mkString.split(',').toSeq.map(_.toInt)
+    val inputData = readResourceFile(inputPath).mkString.split(',').toVector.map(_.toInt)
     val partOneResult = partOne(inputData)
     val partTwoResult = partTwo(inputData)
     assert(partOneResult == partOneAnswer, "Part One Failed")
-    println(s"\nResult for part two: $partTwoResult\n")
-//    println(s"\nResult for part one: \n${partOneResult.toSeq.sortBy(_._1).mkString("\n")}\n")
+    assert(partTwoResult == partTwoAnswer, "Part Two Failed")
   }
 
 }
--- src/test/scala/com/ethang/adventofcode/y2019/Day02Spec.scala
+++ src/test/scala/com/ethang/adventofcode/y2019/Day02Spec.scala
@@ -8,16 +8,13 @@ class Day02Spec extends DaySpec with Inspectors {
   private val testInput = Seq(12, 14, 1969, 100756)
   private val inputPath = "input/y2019/day02.txt"
 
-  private def seqToOpListing(seq: Seq[Int]): Map[Int, Int] = seq.zipWithIndex.map(_.swap).toMap
-
   it should "handle part one test cases" in {
     val testData = Seq(
-      Seq(1, 0, 0, 0, 99) -> Seq(2, 0, 0, 0, 99),
-      Seq(2, 3, 0, 3, 99) -> Seq(2, 3, 0, 6, 99),
-      Seq(2, 4, 4, 5, 99, 0) -> Seq(2, 4, 4, 5, 99, 9801),
-      Seq(1, 1, 1, 4, 99, 5, 6, 0, 99) -> Seq(30, 1, 1, 4, 2, 5, 6, 0, 99)
+      Vector(1, 0, 0, 0, 99) -> Vector(2, 0, 0, 0, 99),
+      Vector(2, 3, 0, 3, 99) -> Vector(2, 3, 0, 6, 99),
+      Vector(2, 4, 4, 5, 99, 0) -> Vector(2, 4, 4, 5, 99, 9801),
+      Vector(1, 1, 1, 4, 99, 5, 6, 0, 99) -> Vector(30, 1, 1, 4, 2, 5, 6, 0, 99)
     )
-        .map { case (l, r) => (seqToOpListing(l), seqToOpListing(r))}
 
     forAll(testData) {
       case (input, result) => Day02.evalProgram(input) must be(result)
