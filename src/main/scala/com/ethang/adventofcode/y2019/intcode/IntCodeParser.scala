63f3445b5e701cc08307dd73a972450a153de527
Ethan Garrison <ethan.garrison@publicisgroupe.com>
2019-12-09 16:07:03 -0500

    Update IntCodeParser to handle I/O

--- src/main/scala/com/ethang/adventofcode/y2019/intcode/IntCodeParser.scala
+++ src/main/scala/com/ethang/adventofcode/y2019/intcode/IntCodeParser.scala
@@ -2,36 +2,48 @@ package com.ethang.adventofcode.y2019.intcode
 
 object IntCodeParser {
 
-  private def evalOpCode(listing: Vector[Int], curr: Int): Vector[Int] = {
+  private type Program = (Vector[Int], Int, Seq[Int])
+
+  private def evalOpCode(listing: Vector[Int], curr: Int, inputs: Seq[Int]): Program = {
     val currVal = listing(curr)
     val opCode = currVal % 100
     val args = (currVal / 100).toString.map(_.asDigit).reverse.toList
 
-    val param = (line: Int, paramMode: Int) => paramMode match {
+    def param(line: Int, paramMode: Int = 1): Int = paramMode match {
       case 0 => listing(listing(line))
       case 1 => listing(line)
     }
 
+    // 1 - Addition
+    // 2 - Multiplication
+    // 3 - Read input
+    // 4 - Write output
+    // TODO: Update output to instead write to a state variable instead of println
     opCode match {
       case 1 | 2 =>
-        val fst :: snd :: assign :: Nil = args.padTo(2, 0).padTo(3, 1)
+        val fst :: snd :: Nil = args.padTo(2, 0)
         val leftArg = param(curr + 1, fst)
         val rightArg = param(curr + 2, snd)
-        val assignLoc = param(curr + 3, assign)
-        val updatedValue = if(opCode == 1) leftArg + rightArg else leftArg * rightArg
-        listing.updated(assignLoc, updatedValue)
-      case 3 | 4 => ???
+        val assignLoc = param(curr + 3)
+        val updatedValue = if (opCode == 1) leftArg + rightArg else leftArg * rightArg
+        (listing.updated(assignLoc, updatedValue), curr + 4, inputs)
+      case 3 => (listing.updated(param(curr + 1), inputs.head), curr + 2, inputs.tail)
+      case 4 =>
+        val fst = args.headOption.getOrElse(0)
+        println(param(curr + 1, fst))
+        (listing, curr + 2, inputs)
       case _ => assert(assertion = false, s"Undefined OpCode $opCode"); ???
     }
   }
 
-  def evalProgram(programListing: Vector[Int]): Vector[Int] = {
+  def evalProgram(programListing: Vector[Int], inputs: Seq[Int] = Seq()): Vector[Int] = {
     @scala.annotation.tailrec
-    def recurse(listing: Vector[Int], line: Int): Vector[Int] = {
-      if(listing(line) == 99) listing
-      else recurse(evalOpCode(listing, line), line + 4)
+    def recurse(program: Program): Program = program match {
+      case (listing, line, _) if listing(line) == 99 => program
+      case (listing, line, inputs) => recurse(evalOpCode(listing, line, inputs))
     }
-    recurse(programListing, 0)
+
+    recurse(programListing, 0, inputs)._1
   }
 
 }
--- src/test/scala/com/ethang/adventofcode/y2019/intcode/IntCodeParserSpec.scala
+++ src/test/scala/com/ethang/adventofcode/y2019/intcode/IntCodeParserSpec.scala
@@ -6,15 +6,24 @@ class IntCodeParserSpec extends BaseSpec {
 
   import com.ethang.adventofcode.y2019.intcode.IntCodeParser._
 
-  it should "handle part one test cases" in {
+  it should "test multiplication/addition" in {
     val testData = Seq(
       Vector(1, 0, 0, 0, 99) -> Vector(2, 0, 0, 0, 99),
       Vector(2, 3, 0, 3, 99) -> Vector(2, 3, 0, 6, 99),
       Vector(2, 4, 4, 5, 99, 0) -> Vector(2, 4, 4, 5, 99, 9801),
-      Vector(1, 1, 1, 4, 99, 5, 6, 0, 99) -> Vector(30, 1, 1, 4, 2, 5, 6, 0, 99)
+      Vector(1, 1, 1, 4, 99, 5, 6, 0, 99) -> Vector(30, 1, 1, 4, 2, 5, 6, 0, 99),
+      Vector(1002, 4, 3, 4, 33) -> Vector(1002, 4, 3, 4, 99)
     )
 
     forAll(testData) { case (input, result) => evalProgram(input) must be(result) }
+  }
+
+  it should "Test I/O" in {
+    val testData = Seq(
+      (Vector(3, 0, 4, 0, 99), Seq(5)) -> Vector(5, 0, 4, 0, 99)
+    )
+
+    forAll(testData) { case ((listing, inputs), result) => evalProgram(listing, inputs) must be(result) }
   }
 
 }
