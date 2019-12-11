df51187360feef9bc9795670de5ba86e0b1a9242
Ethan Garrison <ethang994@gmail.com>
2019-12-11 00:49:13 -0500

    Finish Day07 part two

--- src/main/scala/com/ethang/adventofcode/y2019/Day07.scala
+++ src/main/scala/com/ethang/adventofcode/y2019/Day07.scala
@@ -1,10 +1,11 @@
 package com.ethang.adventofcode.y2019
 
 import com.ethang.adventofcode.y2019.intcode.IntCodeParser
+import com.ethang.adventofcode.y2019.intcode.IntCodeParser.IntCodeProgram
 
 object Day07 extends ProblemFormat[Vector[Int], Int] {
   override val partOneAnswer: Int = 206580
-  override val partTwoAnswer: Int = 0
+  override val partTwoAnswer: Int = 2299406
 
   override def readInput(path: String): Vector[Int] = IntCodeParser.readIntCodeFile(path)
 
@@ -19,5 +20,31 @@ object Day07 extends ProblemFormat[Vector[Int], Int] {
     (for(phaseSettings <- (0 to 4).permutations) yield recurse(phaseSettings)).max
   }
 
-  override def partTwo(inputData: Vector[Int]): Int = 0
+  override def partTwo(inputData: Vector[Int]): Int = {
+    def parseWithFeedback(phaseSettings: Seq[Int]): Int = {
+      @scala.annotation.tailrec
+      def recurse(settings: Seq[Int], ampStateMap: Map[Int, IntCodeProgram], previous: Int): Int = {
+        val amp = settings.head
+        val (listing, line, result) = ampStateMap(amp)
+        val (updated, nextLine, nextResult) = {
+          val (inUpdated, inLine, _) = IntCodeParser.interactiveProgram(listing, line)(Some(previous))
+          IntCodeParser.interactiveProgram(inUpdated, inLine)(None)
+        }
+
+        if (amp == phaseSettings.last && nextResult.isEmpty) result.get
+        else {
+          recurse(
+            if(settings.tail.isEmpty) phaseSettings else settings.tail,
+            ampStateMap.updated(amp, (updated, nextLine, nextResult)),
+            nextResult.orElse(result).get
+          )
+        }
+      }
+
+      val initialState = phaseSettings.map(p => p -> IntCodeParser.interactiveProgram(inputData)(Some(p))).toMap
+      recurse(phaseSettings, initialState, 0)
+    }
+
+    (for(phaseSettings <- (5 to 9).permutations) yield parseWithFeedback(phaseSettings)).max
+  }
 }
