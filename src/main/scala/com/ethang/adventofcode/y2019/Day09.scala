bb1a0955e47bf84cb8aa570a054e8ad4fa782bb7
Ethan Garrison <ethan.garrison@publicisgroupe.com>
2019-12-12 00:41:47 -0500

    Finish Day09 part two

--- src/main/scala/com/ethang/adventofcode/y2019/Day09.scala
+++ src/main/scala/com/ethang/adventofcode/y2019/Day09.scala
@@ -4,11 +4,11 @@ import com.ethang.adventofcode.y2019.intcode.IntCodeParser.{readIntCodeFile, eva
 
 object Day09 extends ProblemFormat[Vector[Long], Long] {
   override val partOneAnswer: Long = 2932210790L
-  override val partTwoAnswer: Long = 0
+  override val partTwoAnswer: Long = 73144L
 
   override def readInput(path: String): Vector[Long] = readIntCodeFile(path)
 
   override def partOne(inputData: Vector[Long]): Long = evalProgram(inputData, Seq(1))._2.head
 
-  override def partTwo(inputData: Vector[Long]): Long = 0
+  override def partTwo(inputData: Vector[Long]): Long = evalProgram(inputData, Seq(2))._2.head
 }
