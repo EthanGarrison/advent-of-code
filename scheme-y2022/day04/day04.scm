594215744fc81c6a1d979676e0ad64e75659c7b3
Ethan Garrison <ethang994@gmail.com>
2024-05-18 17:40:53 -0400

    Add scheme-y2022/day04, add id to utils

---
+++ scheme-y2022/day04/day04.scm
@@ -0,0 +1,41 @@
+; Parse the line into a pair of pairs
+(define parse-line
+  (lambda (str)
+    (let* ((comma-splitter (string-splitter 'delimiter #\,))
+           (dash-splitter (string-splitter 'delimiter #\-))
+           ; Apply comma split, then dash split
+           (full-split-str (map dash-splitter (comma-splitter str))))
+      ; Fish string->number through each value.
+      (>=> string->number full-split-str))))
+
+(define nested-pairs?
+  (lambda (left-pair right-pair)
+    (let ((check (lambda (l r)
+                   (and (>= (car l) (car r))
+                        (<= (cadr l) (cadr r))))))
+      (or (check left-pair right-pair) (check right-pair left-pair)))))
+
+(define overlap-pairs?
+  (lambda (left-pair right-pair)
+    (let ((check (lambda (point pair)
+                   (and (>= point (car pair))
+                        (<= point (cadr pair))))))
+      (or (or (check (car left-pair) right-pair)
+              (check (cadr left-pair) right-pair))
+          (or (check (car right-pair) left-pair)
+              (check (cadr right-pair) left-pair))))))
+
+
+(define part1
+  (lambda (l)
+    (length (filter (lambda (row) (apply nested-pairs? (parse-line row))) l))))
+
+
+(define part2
+  (lambda (l)
+    (length (filter (lambda (row) (apply overlap-pairs? (parse-line row))) l))))
+
+; (with-file-as-list "day04/input.txt" part1)
+
+(with-file-as-list "day04/input.txt" part2)
+
--- scheme-y2022/lib/utils.scm
+++ scheme-y2022/lib/utils.scm
@@ -1,13 +1,28 @@
 (define csort (lambda (fn l) (sort l fn)))
 
+; Kind of hot.  Instead of letrec, can define recursive function with defaults
+; So, recurse has `acc` and `f`, with `'()` and `file` as the initial values
 (define file->list
   (lambda (file)
-    (letrec ((recurse (lambda (acc f)
-                        (cond ((eof-object? (peek-char f)) acc)
-                              (else (recurse (cons (read-line f) acc) f))))))
-      (recurse '() file))))
+    (let recurse ((acc '())
+                  (f file))
+      (if (eof-object? (peek-char f))
+        acc
+        (recurse (cons (read-line f) acc) f)))))
 
 (define with-file-as-list
   (lambda (file fn)
     (call-with-input-file file (lambda (f) (fn (file->list f))))))
+
+; Super basic fish operator.  Definitely not accurate, but works enough for now
+(define >=>
+  (lambda (fn list-of-list)
+    (map (lambda (inner) (map fn inner)) list-of-list)))
+
+(define id (lambda (i) i))
+
+(define compose
+  (lambda (f g)
+    (lambda args
+      (f (apply g args)))))
 
