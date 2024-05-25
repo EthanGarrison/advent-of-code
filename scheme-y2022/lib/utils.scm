46ad57e2ec7057f5bd898870076e33a80c678e86
Ethan Garrison <ethang994@gmail.com>
2024-05-25 18:29:57 -0400

    Loosen grouped, rev result of with-file-as-list.  Probably breaks previous days

--- scheme-y2022/lib/utils.scm
+++ scheme-y2022/lib/utils.scm
@@ -12,7 +12,7 @@
 
 (define with-file-as-list
   (lambda (file fn)
-    (call-with-input-file file (lambda (f) (fn (file->list f))))))
+    (call-with-input-file file (lambda (f) (fn (reverse (file->list f)))))))
 
 ; Super basic fish operator.  Definitely not accurate, but works enough for now
 (define >=>
@@ -25,4 +25,15 @@
   (lambda (f g)
     (lambda args
       (f (apply g args)))))
+
+; Group l into chunks of size i
+; Last elem is not guaranteed to be size i
+; Not tail-recursive, should fix that
+(define grouped
+  (lambda (i l)
+    (cond ((eq? i 0) '())          ; Empty group size, empty result
+          ((eq? i 1) l)            ; Group of one, just return the input
+          ((eqv? l '()) '())       ; Given list is empty, return empty
+          ((< (length l) i) (list l))  ; If there are not enough elements in l, return l.  Only needed because drop is strict
+          (else (cons (take l i) (grouped i (drop l i)))))))
 
